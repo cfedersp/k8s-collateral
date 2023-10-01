f657af6de20d3bfe88277ef9d7bc73a19b3fa112
cfedersp <charles.federspiel@gmail.com>
2023-10-01 15:28:45 -0700

    fixed config script

--- initialize/prep-config.sh
+++ initialize/prep-config.sh
@@ -5,5 +5,5 @@ mkdir -p $NEW_CONFIG_HOME
 cp -i /etc/kubernetes/admin.conf $NEW_CONFIG_HOME/config
 chown $(id -u $1):$(id -g) $NEW_CONFIG_HOME/config
 
-if [[ $2 ]]; then aws s3 cp /etc/kubernetes/admin.conf $2; fi
-curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | { echo -n MASTER_IP=; cat; } | aws s3 cp - s3://baremetal-k8s-no-eks-west-kubernetesconfigbucket-fryh6zd70bj2/master-ip.env
+if [[ $2 ]]; then aws s3 cp /etc/kubernetes/admin.conf $2/config; fi
+curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | { echo -n MASTER_IP=; cat; } | aws s3 cp - s3://$2/master-ip.env
