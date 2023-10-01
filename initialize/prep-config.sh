55d4313acb661c9195aa52cb981fd53f0a582c5b
cfedersp <charles.federspiel@gmail.com>
2023-10-01 12:16:34 -0700

    added optional upload of k8s config to s3

--- initialize/prep-config.sh
+++ initialize/prep-config.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 NEW_CONFIG_HOME=/home/$1/.kube
 mkdir -p $NEW_CONFIG_HOME
 cp -i /etc/kubernetes/admin.conf $NEW_CONFIG_HOME/config
 chown $(id -u $1):$(id -g) $NEW_CONFIG_HOME/config
+
+if [[ $2 ]]; then aws s3 cp /etc/kubernetes/admin.conf $2; fi
 
