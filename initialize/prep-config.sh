2ca94a167cca66b5e0cfdb5f1ae387124db9aa7d
cfedersp <charles.federspiel@gmail.com>
2023-10-01 15:10:37 -0700

    save master ip to s3 config bucket

--- initialize/prep-config.sh
+++ initialize/prep-config.sh
@@ -6,4 +6,4 @@ cp -i /etc/kubernetes/admin.conf $NEW_CONFIG_HOME/config
 chown $(id -u $1):$(id -g) $NEW_CONFIG_HOME/config
 
 if [[ $2 ]]; then aws s3 cp /etc/kubernetes/admin.conf $2; fi
-
+curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | { echo -n MASTER_IP=; cat; } | aws s3 cp - s3://baremetal-k8s-no-eks-west-kubernetesconfigbucket-fryh6zd70bj2/master-ip.env
