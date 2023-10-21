6ea69748cb508f695b8c7cec736eaf4ab372909c
cfedersp <charles.federspiel@gmail.com>
2023-10-21 15:25:52 -0700

    use single env file for join service

--- join/k8s-join.sh
+++ join/k8s-join.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-source /etc/k8s-staging/init.env
-source $(aws s3 cp s3://$CONFIG_BUCKET/master-ip.env -)
 /usr/bin/kubeadm join --discovery-file='/etc/kubernetes/bootstrap-kubelet.conf' --cri-socket=unix:///var/run/crio/crio.sock  --apiserver-advertise-address="$MASTER_IP"
