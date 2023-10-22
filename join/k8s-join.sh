cffd5ac46dd6891963d23be54753e792f535822b
cfedersp <charles.federspiel@gmail.com>
2023-10-22 12:43:25 -0700

    use bash for join script.

--- join/k8s-join.sh
+++ join/k8s-join.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
 /usr/bin/kubeadm join --discovery-file='/etc/kubernetes/bootstrap-kubelet.conf' --cri-socket=unix:///var/run/crio/crio.sock  --apiserver-advertise-address="$MASTER_IP"
