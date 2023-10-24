ab43da1eabe27fbd1ac7c89a5b2539cdf09d66c0
cfedersp <charles.federspiel@gmail.com>
2023-10-23 19:55:53 -0700

    added cri-o wait before kubeadmin init.

--- initialize/k8s.sh
+++ initialize/k8s.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-
+# CRI-o is running but it may not be ready
+export CRIOREADY="crictl ps"
+for STATUSCOUNT in $(seq 1 30); do RESULT=$($CRIOREADY 2>&1); if [[ $? != 0 ]]; then  echo "$STATUSCOUNT $CRIOREADY"; sleep 40;  else echo "crio ready!"; break; fi  done;
+            
 /usr/bin/kubeadm init --pod-network-cidr='10.45.0.0/16' --service-cidr='10.96.0.0/20' --apiserver-advertise-address="$(hostname -i | cut -d " " -f 2)"  --cri-socket=unix:///var/run/crio/crio.sock
 
