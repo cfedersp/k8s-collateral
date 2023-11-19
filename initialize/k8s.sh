a751dec4ffeb2dde12bb03e5c091f0b0f89c152a
cfedersp <charles.federspiel@gmail.com>
2023-11-18 18:06:32 -0800

    added ClusterConfiguration.

--- initialize/k8s.sh
+++ initialize/k8s.sh
@@ -3,5 +3,5 @@
 export CRIOREADY="crictl ps"
 for STATUSCOUNT in $(seq 1 10); do RESULT=$($CRIOREADY 2>&1); if [[ $? != 0 ]]; then  echo "$STATUSCOUNT $CRIOREADY"; sleep 40;  else echo "crio ready!"; break; fi  done;
             
-/usr/bin/kubeadm init --pod-network-cidr='10.45.0.0/16' --service-cidr='10.96.0.0/20' --apiserver-advertise-address="$(hostname -i | cut -d " " -f 2)"  --cri-socket=unix:///var/run/crio/crio.sock
+/usr/bin/kubeadm init --pod-network-cidr='10.45.0.0/16' --service-cidr='10.96.0.0/20' --apiserver-advertise-address="$(hostname -i | cut -d " " -f 2)"  --config /etc/k8s-staging/k8s-collateral/initialize/cluster-config.yaml --cri-socket=unix:///var/run/crio/crio.sock
 
