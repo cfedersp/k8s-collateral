d2967b46faf671ddba6636b9bc26b8aa65295a8c
cfedersp <charles.federspiel@gmail.com>
2023-11-25 16:36:34 -0800

    added SANs

--- initialize/k8s.sh
+++ initialize/k8s.sh
@@ -4,6 +4,8 @@ export CRIOREADY="crictl ps"
 for STATUSCOUNT in $(seq 1 10); do RESULT=$($CRIOREADY 2>&1); if [[ $? != 0 ]]; then  echo "$STATUSCOUNT $CRIOREADY"; sleep 40;  else echo "crio ready!"; break; fi  done;
 
 cp /etc/k8s-staging/k8s-collateral/initialize/cluster-config-base.yaml /etc/k8s-staging/k8s-collateral/initialize/cluster-config.yaml
+printf "\n  - \"$(hostname -i | cut -d " " -f 2)\"" >> /etc/k8s-staging/k8s-collateral/initialize/cluster-config.yaml
 printf "\ncontrolPlaneEndpoint: $(hostname -i | cut -d " " -f 2)" >> /etc/k8s-staging/k8s-collateral/initialize/cluster-config.yaml
+
 /usr/bin/kubeadm init --config /etc/k8s-staging/k8s-collateral/initialize/cluster-config.yaml
 
